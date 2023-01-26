11f11c40ede7286448fa79dd155d6f3167bc6ca3
QueenlyCrimson <ccinemainn@gmail.com>
2023-01-26 10:04:45 -0600

    8 Cards done

--- js/game.js
+++ js/game.js
@@ -6,35 +6,86 @@ let turnNum = 0
 let mPairs = 0
 let msgBoard = document.querySelector('.console')
 let cardCount = 0
-let winCondition = 4
+let winCondition = 6
 
 let cardA1 = {
   ref: document.querySelectorAll('.card')[0],
-  wValue: 0
+  wValue: 0,
+  mValue: 0
 }
 let cardA2 = {
   ref: document.querySelectorAll('.card')[1],
-  wValue: 0
+  wValue: 0,
+  mValue: 0
 }
 let cardB1 = {
   ref: document.querySelectorAll('.card')[2],
-  wValue: 0
+  wValue: 0,
+  mValue: 0
 }
 let cardB2 = {
   ref: document.querySelectorAll('.card')[3],
-  wValue: 0
+  wValue: 0,
+  mValue: 0
 }
+let cardC1 = {
+  ref: document.querySelectorAll('.card')[4],
+  wValue: 0,
+  mValue: 0
+}
+let cardC2 = {
+  ref: document.querySelectorAll('.card')[5],
+  wValue: 0,
+  mValue: 0
+}
+let cardD1 = {
+  ref: document.querySelectorAll('.card')[6],
+  wValue: 0,
+  mValue: 0
+}
+let cardD2 = {
+  ref: document.querySelectorAll('.card')[7],
+  wValue: 0,
+  mValue: 0
+}
+let cardE1 = {
+  ref: document.querySelectorAll('.card')[8],
+  wValue: 0,
+  mValue: 0
+}
+let cardE2 = {
+  ref: document.querySelectorAll('.card')[9],
+  wValue: 0,
+  mValue: 0
+}
+
+let cardArray = [
+  cardA1,
+  cardA2,
+  cardB1,
+  cardB2,
+  cardC1,
+  cardC2,
+  cardD1,
+  cardD2,
+  cardE1,
+  cardE2
+]
+
 let flipFunction = (card) => {
   if (card.wValue == 0) {
-    turnNum++
-    card.ref.classList.toggle('flipped')
-    if (turnNum % 2 == 0) {
-      c2 = card
-      console.log(`This is ${c2.innerText}`)
-      checkFunction(card)
-    } else {
-      c1 = card
-      console.log(`This is ${c1.innerText}`)
+    if (card.mValue == 0) {
+      turnNum++
+      card.ref.classList.toggle('flipped')
+      if (turnNum % 2 == 0) {
+        c2 = card
+        console.log(`This is ${c2.ref.innerText}`)
+        checkFunction(card)
+      } else {
+        c1 = card
+        c1.wValue++
+        console.log(`This is ${c1.ref.innerText}`)
+      }
     }
   }
 }
@@ -45,11 +96,11 @@ let winGame = () => {
 
 let matchCard = () => {
   mPairs++
-  msgBoard.innerText = `Matched Pairs: ${mPairs}`
+  msgBoard.innerText = `Matched Clues: ${mPairs}`
 }
 
 let checkFunction = (card) => {
-  if (c1.innerText == c2.innerText) {
+  if (c1.ref.innerText === c2.ref.innerText) {
     console.log('It worked?')
     cardCount++
     cardCount++
@@ -64,9 +115,15 @@ let checkFunction = (card) => {
     }
   } else {
     console.log('No match')
+    for (let i = 0; i < cardArray.length; i++) {
+      cardArray[i].wValue = 20
+    }
     setTimeout(() => {
-      c1.classList.toggle('flipped')
-      c2.classList.toggle('flipped')
+      c1.ref.classList.toggle('flipped')
+      c2.ref.classList.toggle('flipped')
+      for (let i = 0; i < cardArray.length; i++) {
+        cardArray[i].wValue = 0
+      }
     }, 3000)
   }
 }
@@ -88,10 +145,26 @@ cardB2.ref.addEventListener('click', () => {
   flipFunction(cardB2)
 })
 
-//cardC1.ref.addEventListener('click', () => {
-// flipFunction(cardC1)
-//})
+cardC1.ref.addEventListener('click', () => {
+  flipFunction(cardC1)
+})
 
-//cardC2.ref.addEventListener('click', () => {
-//  flipFunction(cardC2)
-//})
+cardC2.ref.addEventListener('click', () => {
+  flipFunction(cardC2)
+})
+
+cardD1.ref.addEventListener('click', () => {
+  flipFunction(cardD1)
+})
+
+cardD2.ref.addEventListener('click', () => {
+  flipFunction(cardD2)
+})
+
+cardE1.ref.addEventListener('click', () => {
+  flipFunction(cardE1)
+})
+
+cardE2.ref.addEventListener('click', () => {
+  flipFunction(cardE2)
+})
